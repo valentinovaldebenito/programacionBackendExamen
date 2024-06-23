f06f749da31a456a5b8b3ab2251a3283b79018d8
Valentino Valdebenito <144290081+valentinovaldebenito@users.noreply.github.com>
2024-06-23 20:12:17 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app/app");
-
-const port = process.env.port || 3002;
-
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
-});
--- src/router/product.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get("/products", (res, req) => {
-    res.send("I am a Router");
-});
-
-module.exports.router;
